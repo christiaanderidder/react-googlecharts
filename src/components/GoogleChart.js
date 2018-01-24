d26cbdeb57bd5cc80b2d90bc00cb2df9f79b3929
RicardoGeek <ricardo@ricardogeek.com>
2018-01-24 00:39:48 -0600

    Adds ReactDom so findDOMNode works again

--- src/components/GoogleChart.js
+++ src/components/GoogleChart.js
@@ -1,155 +1,158 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
 import Loader from '../lib/GoogleChartsLoader';
 
 export default class GoogleChart extends React.Component {
 
-	constructor() {
-		super();
-		this.state = { 
-			chart: null
-		};
-	}
+        constructor() {
+            super();
+            this.state = {
+                chart: null
+            };
+        }
 
-	handleResize() {
-		if(this.resizeTimer) clearTimeout(this.resizeTimer);
-	    this.resizeTimer = setTimeout(() => this.drawChart(), 100);
-	}
+        handleResize() {
+            if (this.resizeTimer) clearTimeout(this.resizeTimer);
+            this.resizeTimer = setTimeout(() => this.drawChart(), 100);
+        }
 
-	hasData() {
-		return (this.props.data && this.props.data.length > 0);
-	}
+        hasData() {
+            return (this.props.data && this.props.data.length > 0);
+        }
 
-	createChart() {
-		if(!this.hasData() || !this.google) return;
+        createChart() {
+            if (!this.hasData() || !this.google) return;
 
-		console.log('create:' + this.props.type);
+            console.log('create:' + this.props.type);
 
-		var node = React.findDOMNode(this.refs.chart);
-		var charts = this.google.visualization;
-		var chart = null;
-		switch(this.props.type) {
-			// Material charts
-			case 'material-bar':
-			case 'material-column':
-				chart = new google.charts.Bar(node);
-				break;
-			case 'material-line':
-				chart = new google.charts.Line(node);
-				break;
-			case 'material-scatter':
-				chart = new google.charts.Scatter(node);
-				break;
+            var node = ReactDom.findDOMNode(this.refs.chart);
+            var charts = this.google.visualization;
+            var chart = null;
+            switch (this.props.type) {
+                // Material charts
+                case 'material-bar':
+                case 'material-column':
+                    chart = new google.charts.Bar(node);
+                    break;
+                case 'material-line':
+                    chart = new google.charts.Line(node);
+                    break;
+                case 'material-scatter':
+                    chart = new google.charts.Scatter(node);
+                    break;
 
-			// Core charts package
-			case 'area':
-				chart = new charts.AreaChart(node);
-				break;
-			case 'bar':
-				chart = new charts.BarChart(node);
-				break;
-			case 'bubble':
-				chart = new charts.BubbleChart(node);
-				break;
-			case 'candlestick':
-				chart = new charts.CandlestickChart(node);
-				break;
-			case 'column':
-				chart = new charts.ColumnChart(node);
-				break;
-			case 'combo':
-				chart = new charts.ComboChart(node);
-				break;
-			case 'geo':
-				chart = new charts.GeoChart(node);
-				break;
-			case 'histogram':
-				chart = new charts.Histogram(node);
-				break;
-			case 'line':
-				chart = new charts.LineChart(node);
-				break;
-			case 'pie':
-				chart = new charts.PieChart(node);
-				break;
-			case 'scatter':
-				chart = new charts.ScatterChart(node);
-				break;
-			case 'sparkline':
-				chart = new charts.SparklineChart(node);
-				break;
-			case 'stepped-area':
-				chart = new charts.SteppedAreaChart(node);
-				break;
-		}
-		
-		if(chart) {
-			this.setState({
-				chart: chart
-			});
-		}
-	}
+                    // Core charts package
+                case 'area':
+                    chart = new charts.AreaChart(node);
+                    break;
+                case 'bar':
+                    chart = new charts.BarChart(node);
+                    break;
+                case 'bubble':
+                    chart = new charts.BubbleChart(node);
+                    break;
+                case 'candlestick':
+                    chart = new charts.CandlestickChart(node);
+                    break;
+                case 'column':
+                    chart = new charts.ColumnChart(node);
+                    break;
+                case 'combo':
+                    chart = new charts.ComboChart(node);
+                    break;
+                case 'geo':
+                    chart = new charts.GeoChart(node);
+                    break;
+                case 'histogram':
+                    chart = new charts.Histogram(node);
+                    break;
+                case 'line':
+                    chart = new charts.LineChart(node);
+                    break;
+                case 'pie':
+                    chart = new charts.PieChart(node);
+                    break;
+                case 'scatter':
+                    chart = new charts.ScatterChart(node);
+                    break;
+                case 'sparkline':
+                    chart = new charts.SparklineChart(node);
+                    break;
+                case 'stepped-area':
+                    chart = new charts.SteppedAreaChart(node);
+                    break;
+            }
 
-	drawChart() {
-		if(!this.hasData() || !this.google || !this.state.chart) return;
+            if (chart) {
+                this.setState({
+                    chart: chart
+                });
+            }
+        }
 
-		console.log('draw:' + this.props.type);
+        drawChart() {
+            if (!this.hasData() || !this.google || !this.state.chart) return;
 
-		var data = this.google.visualization.arrayToDataTable(this.props.data)
+            console.log('draw:' + this.props.type);
 
-		var options = this.props.options;
-		switch(this.props.type) {
-			case 'material-line':
-				options = this.google.charts.Line.convertOptions(options);
-				break;
-			case 'material-bar':
-			case 'material-column':
-				options = this.google.charts.Bar.convertOptions(options);
-			case 'material-scatter':
-				options = this.google.charts.Scatter.convertOptions(options);
-		}
+            var data = this.google.visualization.arrayToDataTable(this.props.data)
 
-		this.state.chart.draw(data, options);
-	}
+            var options = this.props.options;
+            switch (this.props.type) {
+                case 'material-line':
+                    options = this.google.charts.Line.convertOptions(options);
+                    break;
+                case 'material-bar':
+                case 'material-column':
+                    options = this.google.charts.Bar.convertOptions(options);
+                case 'material-scatter':
+                    options = this.google.charts.Scatter.convertOptions(options);
+            }
 
-	clearChart() {
-		if(!this.hasData() || !this.google || !this.state.chart) return;
+            this.state.chart.draw(data, options);
+        }
 
-		this.state.chart.clearChart();
-	}
-//	shouldComponentUpdate(nextProps, nextState) {
-//		return nextState.chart !== this.state.chart || nextProps.data.length !== this.props.data.length;
-//	}
+        clearChart() {
+                if (!this.hasData() || !this.google || !this.state.chart) return;
 
-	componentDidUpdate() {
-		if(!this.state.chart && this.google && this.hasData()) this.createChart();
-		this.drawChart();
-	}
+                this.state.chart.clearChart();
+            }
+            //	shouldComponentUpdate(nextProps, nextState) {
+            //		return nextState.chart !== this.state.chart || nextProps.data.length !== this.props.data.length;
+            //	}
 
-	componentDidMount() {
-		this.loader = Loader.load();
-		
-		this.loader.then((google) => {
-			this.google = google;
-			this.createChart();
-			this.drawChart();
-		}, (err) => {
-			console.log(err);
-		});
-		window.addEventListener('resize', () => this.handleResize());
-	}
+        componentDidUpdate() {
+            if (!this.state.chart && this.google && this.hasData()) this.createChart();
+            this.drawChart();
+        }
 
-	componentWillUnmount() {
-		window.removeEventListener('resize', () => this.handleResize());
+        componentDidMount() {
+            this.loader = Loader.load();
 
-		this.clearChart();
-	}
+            this.loader.then((google) => {
+                this.google = google;
+                this.createChart();
+                this.drawChart();
+            }, (err) => {
+                console.log(err);
+            });
+            window.addEventListener('resize', () => this.handleResize());
+        }
 
-	render() {
-		if(this.hasData()) {
-			return(<div className="googlechart" ref="chart"></div>);
-		} else {
-			return(<div className="alert alert-danger">No Data</div>);
-		}
-	}
-}
+        componentWillUnmount() {
+            window.removeEventListener('resize', () => this.handleResize());
+
+            this.clearChart();
+        }
+
+        render() {
+                if (this.hasData()) {
+                    return ( < div className = "googlechart"
+                        ref = "chart" > < /div>);
+                    }
+                    else {
+                        return ( < div className = "alert alert-danger" > No Data < /div>);
+                        }
+                    }
+                }
